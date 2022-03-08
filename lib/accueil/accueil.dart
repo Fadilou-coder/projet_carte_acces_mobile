93c830c5a97322169961b47402db780ab790cf86
Fadilou-coder <syfadilou3@gmail.com>
2022-03-08 16:05:36 +0000

    accueil

--- lib/accueil/accueil.dart
+++ lib/accueil/accueil.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:projet_carte_acces/text_with_style.dart';
 
 class Accueil extends StatefulWidget {
+  const Accueil({Key? key}) : super(key: key);
+
   @override
   State<Accueil> createState() => AccueilState();
 }
@@ -10,22 +13,57 @@ class Accueil extends StatefulWidget {
 class AccueilState extends State<Accueil> {
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Image.asset("images/Logo-Sonatel-Academy.png"),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [],
-            )
-          ],
-        ),
+        body: Container(
+      width: size.width,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+              top: 10,
+              child: Image.asset("images/Logo-Sonatel-Academy.png",
+                  width: size.width / 2),
+              height: size.height * 0.5),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            width: size.width,
+            height: size.height * 0.5,
+            child: Card(
+                color: const Color.fromRGBO(19, 138, 138, 100),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      CupertinoButton(
+                          child: Image.asset("images/btn_app.png",
+                              width: size.width / 3),
+                          onPressed: () {}),
+                      CupertinoButton(
+                        child: Image.asset("images/btn_vst.png",
+                            width: size.width / 3),
+                        onPressed: () {},
+                      ),
+                    ]),
+                  ],
+                )),
+          ),
+          Positioned(
+              bottom: 10,
+              child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextWithStyle(
+                      data: "sonatel academy  systeme pointage v1.0",
+                      color: Colors.white,
+                    )
+                  ]))
+        ],
       ),
-    );
+    ));
   }
 }
