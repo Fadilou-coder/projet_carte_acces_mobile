038c8023681ba472db98b001ef1c999f1de4f6c5
Omar faye <omar.fayeomzolive@gmail.com>
2022-05-12 02:12:32 +0000

    carte

--- lib/carteApprenant.dart
+++ lib/carteApprenant.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:odc_pointage/text_with_style.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -17,8 +18,8 @@ class CarteApprenant extends StatefulWidget {
 
 class CarteApprenantState extends State<CarteApprenant> {
 
-  // PickedFile? _imageFile;
-  // final ImagePicker _picker = ImagePicker();
+   //PickedFile? _imageFile;
+   //final ImagePicker _picker = ImagePicker();
 
   late SharedPreferences sharedPreferences;
   var app = new Map<String, dynamic>();
@@ -50,184 +51,183 @@ class CarteApprenantState extends State<CarteApprenant> {
     }
   }
 
-  //Widget imageProfile(){
-  //  return  Center(
-  //  child: Stack(children: <Widget>[
-  //     CircleAvatar(
-  //      radius: 40.0,
-  //   backgroundImage: _imageFile == null
-  //    ? AssetImage("assets/images/test.jpg")
-  //   : FileImage(File(_imageFile.toString())) as ImageProvider,
-  //  ),
-  //  Positioned(
-  //   child: InkWell(
-  //onTap: (){
-  //   showModalBottomSheet(context: context,
-  //     builder: ((builder) => bottomSheet()),
-  //    );
-  //   },
-  //  child: Icon(
-  //    Icons.camera_alt,
-  //     color: Colors.teal,
-  //      size: 20.0,
-  //     ),
-  // ),
-  // ),
-  //  ],
-  // )
-  //);
-//  }
+ // Widget imageProfile(){
+   // return  Center(
+    //child: Stack(children: <Widget>[
+      // CircleAvatar(
+       // radius: 40.0,
+    // backgroundImage: _imageFile == null
+     // ? AssetImage("assets/images/test.jpg") as ImageProvider
+     // : FileImage(File(_imageFile!.path))
+   // ),
+    //Positioned(
+     //child: InkWell(
+      //onTap: (){
+       //  showModalBottomSheet(context: context,
+         //  builder: ((builder) => bottomSheet()),
+          //);
+         //},
+      //child: Icon(
+       // Icons.camera_alt,
+        // color: Colors.teal,
+        //  size: 20.0,
+        // ),
+    // ),
+    // ),
+    //  ],
+    // )
+   // );
+ // }
 
-  //Widget bottomSheet() {
-  //return Container(
-  //height: 40.0,
-  //width: MediaQuery.of(context).size.width,
-  //ctai Column
-  //child: Row(
-  //children: <Widget>[
-  //Text(
-  //"Choose Profile photo",
-  //style: TextStyle(
-  // fontSize: 20.0,
-  // ),
-  //),
-  //Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-  //TextButton.icon(
-  //icon: Icon(Icons.camera),
-  //onPressed: () {
-  //takePhoto(ImageSource.camera);
-  //},
-  //label: Text("Camera"),
-  //),
-  //TextButton.icon(
-  //icon: Icon(Icons.image),
-  //onPressed: () {
-  //takePhoto(ImageSource.gallery);
-  //},
-  // label: Text("Gallery"),
-  // ),
-  // ])
-  // ],
-  // ),
-  // );
-  // }
+ // Widget bottomSheet() {
+   // return Container(
+     //       height: 40.0,
+       //     width: MediaQuery.of(context).size.width,
+         //   child: Row(
+          //  children: <Widget>[
+          //  Text(
+           //   "Choose Profile photo",
+          //     style: TextStyle(
+        //       fontSize: 20.0,
+        //     ),
+         //   ),
+       //     Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+      //      TextButton.icon(
+     //       icon: Icon(Icons.camera),
+  //          onPressed: () {
+  //              takePhoto(ImageSource.camera);
+     //        },
+    //         label: Text("Camera"),
+   //         ),
+   //         TextButton.icon(
+   //         icon: Icon(Icons.image),
+    //        onPressed: () {
+      //         takePhoto(ImageSource.gallery);
+       //     },
+    //         label: Text("Gallery"),
+    //         ),
+    //       ])
+   //        ],
+      //    ),
+      //   );
+     //  }
 
-  //void takePhoto(ImageSource source) async {
-  //final pickedFile = await _picker.pickImage(
-  //  source: source,
-  //);
-  // setState(() {
-  //_imageFile = pickedFile! as PickedFile;
-  //});
-  // }
+     //  void takePhoto(ImageSource source) async {
+     //  final pickedFile = await _picker.pickImage(
+     //    source: source,
+   //   );
+   //    setState(() {
+  //       _imageFile = pickedFile as PickedFile;
+   //   });
+ // }
 
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    if (app.toString() == "{}") {
-      return Scaffold();
-    }
-    return Scaffold(
-      body: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: Stack(alignment: Alignment.center, children: <Widget>[
-            Card(
-                elevation: 24,
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 10,
-                  ),
-                  child: Column(children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          //imageProfile(),
-                          Image.asset("assets/images/logo_odc.png", width: 150),
-                          Image.asset("assets/images/Logo-Sonatel-Academy.png",
-                              width: 120),
-                        ]),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                TextWithStyle(
-                                  data: app['prenom'] + " " + app['nom'],
-                                  size: 16,
-                                  weight: FontWeight.bold,
-                                ),
-                                Row(children: [
+    @override
+    Widget build(BuildContext context) {
+      Size size = MediaQuery.of(context).size;
+      if (app.toString() == "{}") {
+        return Scaffold();
+      }
+      return Scaffold(
+        body: SizedBox(
+            width: size.width,
+            height: size.height,
+            child: Stack(alignment: Alignment.center, children: <Widget>[
+              Card(
+                  elevation: 24,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    child: Column(children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          //  imageProfile(),
+                            Image.asset("assets/images/logo_odc.png", width: 150),
+                            Image.asset("assets/images/Logo-Sonatel-Academy.png",
+                                width: 120),
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
                                   TextWithStyle(
-                                      data: "Numero d'étudiant: ",
-                                      size: 16,
-                                      weight: FontWeight.bold),
-                                  TextWithStyle(data: app['code'], size: 16),
-                                ]),
-                                Row(children: [
-                                  TextWithStyle(
-                                      data: "Réferentiel: ",
-                                      size: 16,
-                                      weight: FontWeight.bold),
-                                  TextWithStyle(
-                                      data: app['referentiel']['libelle'],
-                                      size: 16),
-                                ]),
-                                TextWithStyle(
-                                    data: "Date de naissance:",
+                                    data: app['prenom'] + " " + app['nom'],
                                     size: 16,
-                                    weight: FontWeight.bold),
-                                TextWithStyle(
-                                    data: app['dateNaissance'] +
-                                        " à " +
-                                        app['lieuNaissance'],
-                                    size: 16),
-                                Row(children: [
+                                    weight: FontWeight.bold,
+                                  ),
+                                  Row(children: [
+                                    TextWithStyle(
+                                        data: "Numero d'étudiant: ",
+                                        size: 16,
+                                        weight: FontWeight.bold),
+                                    TextWithStyle(data: app['code'], size: 16),
+                                  ]),
+                                  Row(children: [
+                                    TextWithStyle(
+                                        data: "Réferentiel: ",
+                                        size: 16,
+                                        weight: FontWeight.bold),
+                                    TextWithStyle(
+                                        data: app['referentiel']['libelle'],
+                                        size: 16),
+                                  ]),
                                   TextWithStyle(
-                                      data: "Adresse: ",
+                                      data: "Date de naissance:",
                                       size: 16,
                                       weight: FontWeight.bold),
                                   TextWithStyle(
-                                      data: app['addresse'], size: 16),
+                                      data: app['dateNaissance'] +
+                                          " à " +
+                                          app['lieuNaissance'],
+                                      size: 16),
+                                  Row(children: [
+                                    TextWithStyle(
+                                        data: "Adresse: ",
+                                        size: 16,
+                                        weight: FontWeight.bold),
+                                    TextWithStyle(
+                                        data: app['addresse'], size: 16),
+                                  ]),
+                                  Row(children: [
+                                    TextWithStyle(
+                                        data: "Telephone: ",
+                                        size: 16,
+                                        weight: FontWeight.bold),
+                                    TextWithStyle(data: app['phone'], size: 16),
+                                  ])
                                 ]),
-                                Row(children: [
-                                  TextWithStyle(
-                                      data: "Telephone: ",
-                                      size: 16,
-                                      weight: FontWeight.bold),
-                                  TextWithStyle(data: app['phone'], size: 16),
-                                ])
-                              ]),
-                        ]),
-                  ]),
-                )),
-            Positioned(
-              top: 95,
-              right: 20,
-              child: Image.asset("assets/images/test.jpg", width: 80, height: 80,),
-            ),
-            Positioned(
-              top: 175,
-              right: 10,
-              child: BarcodeWidget(
-                barcode: Barcode.qrCode(),
-                color: Colors.black,
-                data: app['code'],
-                width: 80,
-                height: 60,
+                          ]),
+                    ]),
+                  )),
+              Positioned(
+                top: 95,
+                right: 20,
+                child: Image.asset("assets/images/test.jpg", width: 80, height: 80,),
               ),
-            ),
-            Positioned(
-                bottom: 10,
-                child: Row(children: [
-                  TextWithStyle(
-                      data: "Numero de contact d'urgence: ",
-                      size: 14,
-                      weight: FontWeight.bold),
-                  TextWithStyle(data: app['numTuteur'], size: 14),
-                ]))
-          ])),
-    );
+              Positioned(
+                top: 175,
+                right: 10,
+                child: BarcodeWidget(
+                  barcode: Barcode.qrCode(),
+                  color: Colors.black,
+                  data: app['code'],
+                  width: 80,
+                  height: 60,
+                ),
+              ),
+              Positioned(
+                  bottom: 10,
+                  child: Row(children: [
+                    TextWithStyle(
+                        data: "Numero de contact d'urgence: ",
+                        size: 14,
+                        weight: FontWeight.bold),
+                    TextWithStyle(data: app['numTuteur'], size: 14),
+                  ]))
+            ])),
+      );
+    }
   }
-}
